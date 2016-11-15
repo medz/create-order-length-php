92085de7a0fda6e45e2f17ed6579f42e00fca2a2
Seven <lovevipdsw@outlook.com>
2016-11-15 14:16:06 +0800

    新建一个判断类是否存在的单元测试。

--- test/CreateTest.php
+++ test/CreateTest.php
@@ -3,7 +3,12 @@
 namespace Medz\Component\CreateOrderLength\Test;
 
 use PHPUnit_Framework_TestCase;
+use Medz\Component\CreateOrderLength;
 
 class CreateTest extends PHPUnit_Framework_TestCase
 {
+    public function testClssHas()
+    {
+        $this->assertTrue(class_exists('CreateOrderLength'));
+    }
 }
